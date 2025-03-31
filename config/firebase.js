6157880f80de37d9f33bc6b3372f5f4caa31853a
Krish Mehta <krish1604mehta@gmail.com>
2025-03-31 22:40:23 +0530

    firebase change

--- config/firebase.js
+++ config/firebase.js
@@ -1,29 +1,25 @@
-// config/firebase.js
-const { initializeApp, cert } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
-const { getAuth } = require('firebase-admin/auth');
-const { getStorage } = require('firebase-admin/storage');
+// firebase.js
+const { initializeApp } = require('firebase/app');
+const { getFirestore } = require('firebase/firestore');
+const { getAuth } = require('firebase/auth');
+const { getStorage } = require('firebase/storage');
 
 const firebaseConfig = {
   apiKey: "AIzaSyDr7OADT6n5yC9Ax5BVemxrQGypUFfq7KI",
   authDomain: "industrial-management-sys1604.firebaseapp.com",
   projectId: "industrial-management-sys1604",
-  storageBucket: "industrial-management-sys1604.firebasestorage.app",
+  storageBucket: "industrial-management-sys1604.appspot.com",
   messagingSenderId: "117479346154",
   appId: "1:117479346154:web:f0986e743721226c1c2c93",
   measurementId: "G-6R0HBG1TJD"
 };
 
-// Initialize Firebase Admin
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore
-const db = getFirestore();
-
-// Initialize Firebase Auth
-const auth = getAuth();
-
-// Initialize Firebase Storage
-const storage = getStorage();
+// Initialize Firebase services
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
 
 module.exports = { app, db, auth, storage };
